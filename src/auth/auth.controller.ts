9f88968e71220324656e05b4bc67ca5d1dde9598
Farouk BRAIK <faroukbraik17@gmail.com>
2020-08-25 21:09:25 -0400

    adding signin and signup routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { SignUpDto } from './dto/signup.dto'
+import { SignInDto } from './dto/signin.dto'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @Post('signup')
+  public async signUp(
+    @Body(ValidationPipe) signUpDto: SignUpDto,
+  ): Promise<void> {
+    await this.service.signUp(signUpDto)
+  }
+
+  @Post('signin')
+  public async signIn(
+    @Body(ValidationPipe) signInDto: SignInDto,
+  ): Promise<{ access_token: string }> {
+    return await this.service.signIn(signInDto)
+  }
+}
