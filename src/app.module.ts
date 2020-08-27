03cfec05b62eb85b236a1915815fa701858072e4
Farouk BRAIK <faroukbraik17@gmail.com>
2020-08-26 22:34:04 -0400

    generating playlists module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './config/typeorm.config'
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PlaylistsModule } from './playlists/playlists.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule, PlaylistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
